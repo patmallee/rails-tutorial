a79ef06a81deaec0b05c717fc96f9fed39b7f31d
Patrick Mallee <patmallee@gmail.com>
2016-01-08 02:33:12 +0000

    Add custom routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   
   get 'signup' => 'users#new'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
