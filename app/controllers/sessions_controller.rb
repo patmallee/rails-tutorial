a25b3171a9d646f987eee32a7bef0e50d1ec14b9
Patrick Mallee <patmallee@gmail.com>
2016-01-10 03:39:43 +0000

    Add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    
   end
   
   def create
@@ -15,5 +14,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
