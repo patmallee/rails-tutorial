1648ff78240d2c1269b08212bf273353ae2b51cc
Patrick Mallee <patmallee@gmail.com>
2016-01-18 21:02:36 +0000

    Add dependent destroy to User-Micropost association

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   
   def setup
     @user = users(:michael)
-    @micropost = @user.post.build(content: "Lorem Ipsum")
+    @micropost = @user.microposts.build(content: "Lorem Ipsum")
   end
   
   test 'should be valid' do
@@ -24,6 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
   test 'content should be 140 characters or shorter' do
     @micropost.content = 'a' * 141
     assert_not @micropost.valid?
+  end
+  
+  test 'order should be most recent first' do
+    assert_equal microposts(:most_recent), Micropost.first
   end
   
 end
