e584046e6ef7b1dbfc71ab9122ac0804e6ad4c49
Patrick Mallee <patmallee@gmail.com>
2016-01-18 20:44:50 +0000

    Minor refactoring to use model association

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.post.build(content: "Lorem Ipsum")
   end
   
   test 'should be valid' do
