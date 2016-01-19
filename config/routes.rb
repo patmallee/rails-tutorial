425b175e70d23cf2d76ed7e4ff6cbb1a976313a0
Patrick Mallee <patmallee@gmail.com>
2016-01-19 03:09:22 +0000

    Add micropost creating form to home page

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   #   resources :products
   
   resources :users
+  resources :microposts, only: [:create, :destroy]
   
   # Example resource route with options:
   #   resources :products do
