b45037ee8b04371d5bf2ce21c5523390c46c0c40
Patrick Mallee <patmallee@gmail.com>
2016-01-29 02:31:49 +0000

    Add client- and server-side validations to picture uploads

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,19 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  
+
   mount_uploader :picture, PictureUploader
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+  
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "Should be les than 5MB")
+      end
+    end
+
 end
