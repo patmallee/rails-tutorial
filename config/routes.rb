d97fa5a523a16ffc74371192808cfbe49d556d2f
Patrick Mallee <patmallee@gmail.com>
2016-02-01 02:16:07 +0000

    Add follow/unfollow button to User#show view

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     end
   end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
   # Example resource route with options:
   #   resources :products do
