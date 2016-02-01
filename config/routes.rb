5099152bcebf52351e8ce845a451ddf7f889a8c5
Patrick Mallee <patmallee@gmail.com>
2016-02-01 02:07:21 +0000

    Add stats to home page and User#show view

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
   
   # Example resource route with options:
