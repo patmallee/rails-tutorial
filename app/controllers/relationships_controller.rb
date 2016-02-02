2c08a42df057569debfa400e3b41eddbd37baead
Patrick Mallee <patmallee@gmail.com>
2016-02-02 23:47:12 +0000

    Fix unfollow functionality

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,7 +8,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:followed_id])
+    user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
   end
